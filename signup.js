eef3d38c7d83c6b59aaf712d06e0b4fbaf2eee9f
John Chimezie <mahzeejohn@gmail.com>
2022-12-21 21:33:04 +0100

    added signup module

---
+++ signup.js
@@ -0,0 +1,14 @@
+import {signup} from "../../cypress/fixtures/selectors.js"
+
+describe('GIVEN I AM IN SIGNUP PAGE', function () {
+    beforeEach(function () {
+        cy.visit('/')
+
+    });
+
+    it('I SHOULD BE ABLE TP SIGNUP', function (){
+
+
+    })
+
+})
